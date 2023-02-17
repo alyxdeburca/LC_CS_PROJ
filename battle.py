699a157c6538a08a00401874fa7ec5a1aacfc509
Alyx Burke <alyx@alyx.site>
2023-02-17 14:34:41 +0000

    Fixed Recursion Error

--- battle.py
+++ battle.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 import pygame
 import sys
+import random
 from pokeclass import Pokemon
 
 # Set up Pygame and the window
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
-self = None
+
 # Load the player's party of Pokémon
-player_party = [Pokemon.Pikachu(self), Pokemon.Squirtle(self), Pokemon.Charmander(self)]
-player_party[0].level = 20
+player_party = [Pokemon.Pikachu(), Pokemon.Squirtle(), Pokemon.Charmander()]
 # Load the opponent's party of Pokémon
-opponent_party = [Pokemon.Bulbasaur(self), Pokemon.Caterpie(self), Pokemon.Weedle(self)]
+opponent_party = [Pokemon.Bulbasaur(), Pokemon.Caterpie(), Pokemon.Weedle()]
 # Set up the current Pokémon for each trainer
 player_pokemon = player_party[0]
 opponent_pokemon = opponent_party[0]
-
-
-# 25, 7, 4, 1, 10, 13
 
 def draw_pokemon(pokemon, position):
 	# Load the Pokémon's sprite
@@ -28,36 +25,60 @@ def draw_pokemon(pokemon, position):
 
 	# Blit the sprite onto the screen at the specified position
 	screen.blit(sprite, position)
-
-
 # Set up the battle loop
 while True:
 	opponent_party[0].level = 100
+	player_party[0].level = 100
+
 	# Handle events
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-		if event.type == pygame.KEYDOWN:
+		if event.type == pygame.KEYUP:
 			# Check for player input to select a move
 			if event.key == pygame.K_1:
 				player_pokemon.use_move(player_pokemon.moves[0], opponent_pokemon)
+				if opponent_pokemon.is_fainted():
+					print(f'{opponent_pokemon.name} has fainted!')
+				else:
+					move = random.choice(opponent_pokemon.moves)
+					attacker = player_pokemon
+					opponent_pokemon.use_move(move, attacker)
 				player_pokemon.update()
 				opponent_pokemon.update()
 			if event.key == pygame.K_2:
 				player_pokemon.use_move(player_pokemon.moves[1], opponent_pokemon)
+				if opponent_pokemon.is_fainted():
+					print(f'{opponent_pokemon.name} has fainted!')
+				else:
+					move = random.choice(opponent_pokemon.moves)
+					attacker = player_pokemon
+					opponent_pokemon.use_move(move, attacker)
 				player_pokemon.update()
 				opponent_pokemon.update()
 			if event.key == pygame.K_3:
 				player_pokemon.use_move(player_pokemon.moves[2], opponent_pokemon)
+				if opponent_pokemon.is_fainted():
+					print(f'{opponent_pokemon.name} has fainted!')
+				else:
+					move = random.choice(opponent_pokemon.moves)
+					attacker = player_pokemon
+					opponent_pokemon.use_move(move, attacker)
 				player_pokemon.update()
 				opponent_pokemon.update()
 			if event.key == pygame.K_4:
 				player_pokemon.use_move(player_pokemon.moves[3], opponent_pokemon)
+				if opponent_pokemon.is_fainted():
+					print(f'{opponent_pokemon.name} has fainted!')
+				else:
+					move = random.choice(opponent_pokemon.moves)
+					attacker = player_pokemon
+					opponent_pokemon.use_move(move, attacker)
 				player_pokemon.update()
 				opponent_pokemon.update()
+		
 	# Update the game state
-
 	# Check if either Pokémon has fainted
 	if player_pokemon.is_fainted():
 		# Check if the player has any remaining Pokémon
@@ -70,7 +91,6 @@ while True:
 			break
 	if opponent_pokemon.is_fainted():
 		# Check if the opponent has any remaining Pokémon
-		print(opponent_pokemon.name + " Fainted!")
 		if len(opponent_party) > 1:
 			opponent_party.pop(0)
 			opponent_pokemon = opponent_party[0]
@@ -78,10 +98,12 @@ while True:
 			# The player has won the battle
 			print("You have defeated the opponent!")
 			break
-
+	
 	# Render the battle screen
 	screen.fill((255, 255, 255))
 	draw_pokemon(player_pokemon, (50, 50))
 	draw_pokemon(opponent_pokemon, (550, 50))
+	if opponent_pokemon.is_fainted():
+		print(opponent_pokemon.name + " Fainted!")
 
 	pygame.display.flip()
--- pokeclass.py
+++ pokeclass.py
@@ -1,189 +1,166 @@
 import random
-
-
 class Pokemon:
-	def __init__(self, name, level, sprite_path):
-		self.name = name
-		self.level = level
-		self.sprite_path = sprite_path
-		self.moves = []
-		self.stats = {
-			"hp": 0,
-			"attack": 0,
-			"defense": 0,
-			"sp_attack": 0,
-			"sp_defense": 0,
-			"speed": 0
-		}
+    def __init__(self, name, level, sprite_path):
+        self.name = name
+        self.level = level
+        self.sprite_path = sprite_path
+        self.moves = []
+        self.stats = {
+            "hp": 0,
+            "attack": 0,
+            "defense": 0,
+            "sp_attack": 0,
+            "sp_defense": 0,
+            "speed": 0
+        }
+        
+    def use_move(self, move, target):
+        if move in self.moves:
+            attack_stat = self.stats["attack"] if move.category == "Physical" else self.stats["sp_attack"]
+            defense_stat = target.stats["defense"] if move.category == "Physical" else target.stats["sp_defense"]
+            damage = (((2 * self.level / 5 + 2) * move.power * attack_stat / defense_stat) / 50) + 2
+            target.stats["hp"] = max(0, target.stats["hp"] - damage)
+            print(f"{self.name} used {move.name} on {target.name} and did {damage} damage.")
+        else:
+            print(f"{self.name} does not know {move.name}.")
 
-	def use_move(self, move, target):
-		if move in self.moves:
-			attack_stat = self.stats["attack"] if move.category == "Physical" else self.stats["sp_attack"]
-			defense_stat = target.stats["defense"] if move.category == "Physical" else target.stats["sp_defense"]
-			damage = (((2 * self.level / 5 + 2) * move.power * attack_stat / defense_stat) / 50) + 2
-			target.stats["hp"] = max(0, target.stats["hp"] - damage)
-			print(f"{self.name} used {move.name} on {target.name} and did {damage} damage.")
-			if target.is_fainted():
-				print(f"{target.name} has fainted.")
-			else:
-				# Choose a random move from the target's moves list
-				move = random.choice(target.moves)
-				# Pass the original attacker as the target
-				attacker = self
-				target.use_move(move, attacker)
-		else:
-			print(f"{self.name} does not know {move.name}.")
 
-	def Pikachu(self):
-		self = Pokemon("Pikachu", 5, "sprites/pikachu.png")
-		self.moves = [Thunderbolt(), QuickAttack(), TailWhip(), ThunderWave()]
-		self.stats = {
-			"hp": 35,
-			"attack": 55,
-			"defense": 40,
-			"sp_attack": 50,
-			"sp_defense": 50,
-			"speed": 90
-		}
-		return self
 
-	def Squirtle(self):
-		self = Pokemon("Squirtle", 5, "sprites/squirtle.png")
-		self.moves = [Tackle(), TailWhip(), WaterGun(), Bubble()]
-		self.stats = {
-			"hp": 44,
-			"attack": 48,
-			"defense": 65,
-			"sp_attack": 50,
-			"sp_defense": 64,
-			"speed": 43
-		}
-		return self
 
-	def Charmander(self):
-		self = Pokemon("Charmander", 5, "sprites/charmander.png")
-		self.moves = [Scratch(), Growl(), Ember(), MetalClaw()]
-		self.stats = {
-			"hp": 39,
-			"attack": 52,
-			"defense": 43,
-			"sp_attack": 60,
-			"sp_defense": 50,
-			"speed": 65
-		}
-		return self
+        
+    def Pikachu():
+        pikachu = Pokemon("Pikachu", 5, "sprites/pikachu.png")
+        pikachu.moves = [Thunderbolt(), QuickAttack(), TailWhip(), ThunderWave()]
+        pikachu.stats = {
+            "hp": 35,
+            "attack": 55,
+            "defense": 40,
+            "sp_attack": 50,
+            "sp_defense": 50,
+            "speed": 90
+        }
+        return pikachu
 
-	def Bulbasaur(self):
-		self = Pokemon("Bulbasaur", 5, "sprites/bulbasaur.png")
-		self.moves = [Tackle(), Growl(), RazorLeaf(), SleepPowder()]
-		self.stats = {
-			"hp": 45,
-			"attack": 49,
-			"defense": 49,
-			"sp_attack": 65,
-			"sp_defense": 65,
-			"speed": 45
-		}
-		return self
+    def Squirtle():
+        squirtle = Pokemon("Squirtle", 5, "sprites/squirtle.png")
+        squirtle.moves = [Tackle(), TailWhip(), WaterGun(), Bubble()]
+        squirtle.stats = {
+            "hp": 44,
+            "attack": 48,
+            "defense": 65,
+            "sp_attack": 50,
+            "sp_defense": 64,
+            "speed": 43
+        }
+        return squirtle
 
-	def Caterpie(self):
-		self = Pokemon("Caterpie", 5, "sprites/caterpie.png")
-		self.moves = [Tackle(), StringShot()]
-		self.stats = {
-			"hp": 45,
-			"attack": 30,
-			"defense": 35,
-			"sp_attack": 20,
-			"sp_defense": 20,
-			"speed": 45
-		}
-		return self
+    def Charmander():
+        charmander = Pokemon("Charmander", 5, "sprites/charmander.png")
+        charmander.moves = [Scratch(), Growl(), Ember(), MetalClaw()]
+        charmander.stats = {
+            "hp": 39,
+            "attack": 52,
+            "defense": 43,
+            "sp_attack": 60,
+            "sp_defense": 50,
+            "speed": 65
+        }
+        return charmander
 
-	def Weedle(self):
-		self = Pokemon("Weedle", 5, "sprites/weedle.png")
-		self.moves = [PoisonSting(), StringShot()]
-		self.stats = {
-			"hp": 40,
-			"attack": 35,
-			"defense": 30,
-			"sp_attack": 20,
-			"sp_defense": 20,
-			"speed": 50
-		}
-		return self
+    def Bulbasaur():
+        bulbasaur = Pokemon("Bulbasaur", 5, "sprites/bulbasaur.png")
+        bulbasaur.moves = [Tackle(), Growl(), RazorLeaf(), SleepPowder()]
+        bulbasaur.stats = {
+            "hp": 45,
+            "attack": 49,
+            "defense": 49,
+            "sp_attack": 65,
+            "sp_defense": 65,
+            "speed": 45
+        }
+        return bulbasaur
 
-	def update(self):
-		# Update the Pokémon's state or any other game logic here
-		pass
+    def Caterpie():
+        caterpie = Pokemon("Caterpie", 5, "sprites/caterpie.png")
+        caterpie.moves = [Tackle(), StringShot()]
+        caterpie.stats = {
+            "hp": 45,
+            "attack": 30,
+            "defense": 35,
+            "sp_attack": 20,
+            "sp_defense": 20,
+            "speed": 45
+        }
+        return caterpie
 
-	def is_fainted(self):
-		return self.stats["hp"] == 0
+    def Weedle():
+        weedle = Pokemon("Weedle", 5, "sprites/weedle.png")
+        weedle.moves = [PoisonSting(), StringShot()]
+        weedle.stats = {
+            "hp": 40,
+            "attack": 35,
+            "defense": 30,
+            "sp_attack": 20,
+            "sp_defense": 20,
+            "speed": 50
+        }
+        return weedle
 
+    def update(self):
+        # Update the Pokémon's state or any other game logic here
+        pass
+
+    def is_fainted(self):
+        return self.stats["hp"] == 0
 
 class Move:
-	def __init__(self, name, type, category, power, accuracy):
-		self.name = name
-		self.type = type
-		self.category = category  # Added category attribute
-		self.power = power
-		self.accuracy = accuracy
-
-
+    def __init__(self, name, type, category, power, accuracy):
+        self.name = name
+        self.type = type
+        self.category = category  # Added category attribute
+        self.power = power
+        self.accuracy = accuracy
 def Thunderbolt():
-	return Move("Thunderbolt", "Electric", "Special", 90, 100)
-
+    return Move("Thunderbolt", "Electric", "Special", 90, 100)
 
 def QuickAttack():
-	return Move("Quick Attack", "Normal", "Physical", 40, 100)
-
+    return Move("Quick Attack", "Normal", "Physical", 40, 100)
 
 def TailWhip():
-	return Move("Tail Whip", "Normal", "Status", 0, 100)
-
+    return Move("Tail Whip", "Normal", "Status", 0, 100)
 
 def ThunderWave():
-	return Move("Thunder Wave", "Electric", "Status", 0, 100)
-
+    return Move("Thunder Wave", "Electric", "Status", 0, 100)
 
 def Tackle():
-	return Move("Tackle", "Normal", "Physical", 50, 100)
-
+    return Move("Tackle", "Normal", "Physical", 50, 100)
 
 def WaterGun():
-	return Move("Water Gun", "Water", "Special", 40, 100)
-
+    return Move("Water Gun", "Water", "Special", 40, 100)
 
 def Bubble():
-	return Move("Bubble", "Water", "Special", 40, 100)
-
+    return Move("Bubble", "Water", "Special", 40, 100)
 
 def Growl():
-	return Move("Growl", "Normal", "Status", 0, 100)
-
+    return Move("Growl", "Normal", "Status", 0, 100)
 
 def Ember():
-	return Move("Ember", "Fire", "Special", 40, 100)
-
+    return Move("Ember", "Fire", "Special", 40, 100)
 
 def MetalClaw():
-	return Move("Metal Claw", "Steel", "Physical", 50, 95)
-
+    return Move("Metal Claw", "Steel", "Physical", 50, 95)
 
 def Scratch():
-	return Move("Scratch", "Normal", "Physical", 40, 100)
-
+    return Move("Scratch", "Normal", "Physical", 40, 100)
 
 def RazorLeaf():
-	return Move("Razor Leaf", "Grass", "Physical", 55, 95)
-
+    return Move("Razor Leaf", "Grass", "Physical", 55, 95)
 
 def SleepPowder():
-	return Move("Sleep Powder", "Grass", "Status", 0, 75)
-
+    return Move("Sleep Powder", "Grass", "Status", 0, 75)
 
 def StringShot():
-	return Move("String Shot", "Bug", "Status", 0, 95)
-
+    return Move("String Shot", "Bug", "Status", 0, 95)
 
 def PoisonSting():
-	return Move("Poison Sting", "Poison", "Physical", 15, 100)
+    return Move("Poison Sting", "Poison", "Physical", 15, 100)
