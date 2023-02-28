f9283b5007a2eb36b1ec34ea0bbd69928b8cce6f
Alyx Burke <alyx@alyx.site>
2023-02-28 15:20:47 +0000

    FINISHED DA GAME

--- main.py
+++ main.py
@@ -2,22 +2,7 @@ import input
 import pokemon
 import pygame
 import menu
-
-
-def main():
-	running = True
-	pygame.display.flip()
-	while running:
-		main_menu.draw(screen)
-		mouseObj.get_pos()
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:  # Not technically an input but is grabbed as an event in the same way
-				running = False
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				mouseObj.grab()
-		pygame.display.flip()
-
-
+import font
 class Player(pokemon.Pokemon):
 	def __init__(self, health, player, accuracy, entry):
 		super().__init__(health, player, accuracy, entry)
@@ -40,6 +25,24 @@ FRAME_PER_SECOND_CLOCK.tick(60)
 mouseObj = input.Mouse(screen)
 main_menu = menu.Menu(screen)
 main_menu.draw(screen)
+
+
+def main():
+	running = True
+	pygame.display.flip()
+	while running:
+		font.drawText("POKEMON", "assets/fonts/Font.png", (170, 50), screen, 1)
+		font.drawText("Battle", "assets/fonts/Font.png", (210, 140), screen, 1)
+		font.drawText("Simulator","assets/fonts/Font.png", (80, 230), screen, 1)
+		main_menu.draw(screen)
+		mouseObj.get_pos()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:  # Not technically an input but is grabbed as an event in the same way
+				running = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				mouseObj.grab()
+		pygame.display.flip()
+
 
 if __name__ == "__main__":
 	main()
